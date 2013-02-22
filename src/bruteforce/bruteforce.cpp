142cb93e6d82d50b895e468fc4125ff7ab1f0d15
Sune Marcher <sm@flork.dk>
2013-02-23 00:00:10 +0100

    + added bruteforce project skeleton

---
+++ src/bruteforce/bruteforce.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+
+int main(int argc, char* argv[])
+{
+	if(argc < 2)
+	{
+		puts("bruteforce [sha256-hash] - tries to find a CPR number that matches your hash");
+		return 0;
+	}
+
+	return 0;
+}
