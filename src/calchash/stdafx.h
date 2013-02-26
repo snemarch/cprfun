ed5cf446df4d7e733e1572807acb9c9b2112428a
Sune Marcher <sm@flork.dk>
2013-02-27 00:08:20 +0100

    +added dbtool, initial project layout. * calchash, fixed cstdio->iostream stdafx include.

--- src/calchash/stdafx.h
+++ src/calchash/stdafx.h
@@ -2,8 +2,8 @@
 #ifndef NO_PRECOMPILED_HEADERS			// so we can verify clean module deps.
 
 // C/C++ standard libraries
+#include <iostream>
 #include <string>
-#include <cstdio>
 
 // project includes
 #include <core/core.h>
