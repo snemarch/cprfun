1bfeba646dc1eee1ee7bb012d1459bd7e8bf1342
Sune Marcher <sm@flork.dk>
2025-02-08 22:07:09 +0100

    feat(multibrute): add progress reporting
    Slightly hackily done, adding a global with the thread parameters...
    The codebase could generally use a good refactor and cleanups applying
    modern C++ style.

--- src/multibrute/multibrute.cpp
+++ src/multibrute/multibrute.cpp
@@ -2,6 +2,7 @@
 #include <atomic>
 #include <cmath>
 #include <cstdint>
+#include <iomanip>
 #include <iostream>
 #include <thread>
 #include <vector>
@@ -15,37 +16,55 @@
 using namespace cprfun;
 using namespace std;
 
-atomic<bool> g_stopSearching(false);
-
 struct threadparam_t 
 {
-	const Hash&	targetHash;
-	uint32_t	start;
-	uint32_t	count;
-	int			threadnum;
+	const Hash				*targetHash;
+	atomic_uint_fast32_t	progress;
+	const uint_fast32_t		start;
+	const uint_fast32_t		count;
+	const unsigned			threadnum;
 };
 
-static void bruteforce(const threadparam_t& param)
+//TODO: get rid of the globals.
+bool debug = false;
+vector<unique_ptr<threadparam_t>> params;
+atomic<bool> g_stopSearching(false);
+
+static void bruteforce(threadparam_t *param)
 {
-	cout << "Thread " << param.threadnum << 
-			": scanning for hash " << param.targetHash.toString() 	<< 
-			", range " << param.start << "-" << (param.start + param.count) << endl;
+	if(debug) {
+		cout << "Thread " << param->threadnum <<
+				": scanning for hash " << param->targetHash->toString() 	<<
+				", range " << param->start << "-" << (param->start + param->count) << endl;
+	}
 
 	sha256 sha;
-	runpermutations(param.start, param.count, false, [&](const char *cpr) -> bool {
-		if(const Hash currentHash(sha, cpr, 10); currentHash == param.targetHash)
+	uint_fast32_t count;
+	runpermutations(param->start, param->count, false, [&](const char *cpr) -> bool {
+		// Ugh, this is a bit hacky... so, the "count" param is the number of years the thread processes,
+		// so we need to divide by 366 (the number of always-with-leap days in a year) to do percentage
+		// calculations... we might as well throw in a bit of a micro-optimization: atomics are somewhat
+		// expensive to use, so only update every Xxx iterations.
+		if ((count++ % 366'000) == 0) {
+			param->progress += 1'000;
+		}
+
+		if(const Hash currentHash(sha, cpr, 10); currentHash == *param->targetHash)
 		{
-			cout << "Thread " << param.threadnum << ": got a match! CPR == " << cpr << endl;
+			cout << endl << "Thread " << param->threadnum << ": got a match! CPR == " << cpr << endl;
 			g_stopSearching = true;
 		}
 		return g_stopSearching;
 	});
 
-	cout << "Thread " << param.threadnum << ": exhausted keyspace slice" << endl;
+	if (debug) {
+		cout << "Thread " << param->threadnum << ": exhausted keyspace slice" << endl;
+	}
 }
 
 static vector<thread> createAndLaunchWorkers(uint8_t numThreads, const Hash& targetHash);
 static void waitForWorkers(vector<thread>& workers);
+static void reportProgress(const vector<unique_ptr<threadparam_t>> & vector);
 
 int main(int argc, char *argv[])
 {
@@ -66,6 +85,7 @@ int main(int argc, char *argv[])
 
 		vector<thread> threads = createAndLaunchWorkers(static_cast<uint8_t>(numThreads), targetHash);
 		cout <<  "Worker threads launched, waiting for completion - this may take a while!" << endl;
+		reportProgress(params);
 		waitForWorkers(threads);
 	}
 	catch(const invalid_argument& ex) {
@@ -90,18 +110,34 @@ static vector<thread> createAndLaunchWorkers(uint8_t numThreads, const Hash& tar
 	const auto slicesize = static_cast<unsigned>( ceil(static_cast<double>(keyspace) / numThreads) );
 
 	vector<thread> workers;
-	for(uint8_t t=0; t<numThreads; ++t)
+	for(auto t=0; t<numThreads; ++t)
 	{
-		threadparam_t param{
-			.targetHash = targetHash,
-			.start = t * slicesize,
-			.count = min(slicesize, (keyspace - param.start)),
-			.threadnum = t
-		};
+		auto& p = params.emplace_back(make_unique<threadparam_t>(
+			&targetHash,
+			0,
+			t * slicesize,
+			min(slicesize, (keyspace - (t * slicesize))),
+			t
+		));
 
-		workers.push_back( thread(bruteforce, param) );
+		workers.emplace_back( bruteforce, p.get() );
 	}
+
 	return workers;
+}
+
+static void reportProgress(const vector<unique_ptr<threadparam_t>>& vector)
+{
+	while (!g_stopSearching)
+	{
+		cout << "\r";
+		for(auto& t : vector)
+		{
+			cout << fixed << setprecision(1) << (t->progress * 100.0 / t->count) << " ";
+		}
+		cout << flush;
+		this_thread::sleep_for(chrono::milliseconds(200));
+	}
 }
 
 static void waitForWorkers(vector<thread>& workers)
