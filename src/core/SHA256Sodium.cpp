8d74d16c02dc5a3c50fec15bbc9b8599593ff62e
Sune Marcher <sm@flork.dk>
2025-02-17 01:04:55 +0100

    feat(sha256impl): actually use libsodium, and only compile if enabled

--- src/core/SHA256Sodium.cpp
+++ src/core/SHA256Sodium.cpp
@@ -1,29 +1,28 @@
 #include "stdafx.h"
 #include "SHA256.h"
 
-#include "SHA256Sodium.h""
-#if FEAT_LIBSODIUM
+#include "SHA256Sodium.h"
 #include "sodium.h"
-#endif
 
 namespace cprfun::Sodium {
 
-struct hash_state {
-};
+struct hash_state: crypto_hash_sha256_state { };
 
-SHA256Sodium::SHA256Sodium() {
+SHA256Sodium::SHA256Sodium() : state(std::make_unique<hash_state>()) {
 }
 
-SHA256Sodium::~SHA256Sodium() {
-}
+SHA256Sodium::~SHA256Sodium() = default;
 
 void SHA256Sodium::reset() {
+    crypto_hash_sha256_init(state.get());
 }
 
 void SHA256Sodium::update(const void *buffer, size_t length) {
+    crypto_hash_sha256_update(state.get(), static_cast<const unsigned char *>(buffer), length);
 }
 
 void SHA256Sodium::digest(digest_t& digest) {
+    crypto_hash_sha256_final(state.get(), digest.data());
 }
 
-} // namespace cprfun
+} // namespace cprfun::Sodium
